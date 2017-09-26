332e77f716c44ed34d7e8249d86bc22c59c3f0c9
lvermue <lvermue@users.noreply.github.com>
2017-09-26 11:21:39 +0200

    Update ex3_2.py

--- Week3/ex3_2.py
+++ Week3/ex3_2.py
@@ -2,50 +2,42 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import root
 
-# read file containing points
-with open('points', 'r') as file:
+# read list of points file
+with open('list-of-points-file', 'r') as file:
     # split each line into two strings, convert each one to float
     points = [[float(x) for x in point.split(' ')] for point in file.readlines()]
 
 # convert list of floats to np array
 M = np.asarray(points)
-# show the points
+# Plot the points
 plt.plot(M[:, 0], M[:, 1], 'bo')
 
-
-# ----------------------------------------- #
-# fit 3rd degree polynomial
-# ----------------------------------------- #
+# Using the numpy polyfit function to fit the points
+# to a 3rd degree polynomial function
 coeffs = np.polyfit(M[:, 0], M[:, 1], deg=3)
 
-# ----------------------------------------- #
-# define function to minimize
-# ----------------------------------------- #
-
-
+# Defining the function that calculates y, given a point x 
+# and the coefficient that were estimated by the polyfit function above
 def f(x, coeff):
     y = [x**i * coeff[i] for i in range(len(coeff))]
     return sum(y)
 
 # plot function on the interval [-20,20]
 x = np.linspace(-20, 20, 100)
+#Calculate y by calling the function defined above
 y = f(x, coeffs)
+# Plot the results
 plt.plot(x, y)
 
-
-# ----------------------------------------- #
 # find the roots
-# ----------------------------------------- #
-
 # 3rd degree polynomial, so there may be
-#   1 root or
-#   1 saddle point root or
-#   3 roots
-# initial guess
+#   1 root or 1 saddle point root or 3 roots
+# creating an inital guess initial guess
 x0 = [-10, 0.0, 10]
+# calling the scipy optimize root function, which approximates the roots of the
+# function f
 res = root(f, x0, coeffs)
 
+# Print the approximated roots of the function
 for x in res.x:
-    print("x = {0:3.2f}\t f(x) = {1:3.2f}".format(x, f(x, coeffs)))
-
-
+    print("x = {0:5.3f}\t f(x) = {1:5.3f}".format(x, f(x, coeffs)))
